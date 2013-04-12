1d647d88230b07ae30afd303658faf39cdc5b87e
Jordan Pryde <prydom@gmail.com>
2013-04-12 00:15:54 -0700

    Optimizations
    Made some optimizations, the most important being the use of
    pygame.event.wait() and the use of a surface for drawing the
    instructions.

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ WHITE = (255, 255, 255)
 RED = (255,   0,   0)
 GREEN = (  0, 255,   0)
 BLUE = (  0,   0, 255)
+ALPHA =(  0,   0,   0,   0)
 
 # init all modules with defualt settings
 pygame.init()
 
 def main():
     # get a pygame.surface for the window
-    DISPLAYSURF = pygame.display.set_mode((400,300))
+    DISPLAYSURF = pygame.display.set_mode((400,120))
     # set the window title
     pygame.display.set_caption("DI-SkitBoard")
     mainLoop(DISPLAYSURF)
@@ -29,17 +30,19 @@ def mainLoop(surface):
     running = True
     #display some instructions
     fontObj = pygame.font.Font('C:\Windows\Fonts\lucon.TTF', 12) # this causes a windows dependence!!!
-    textRect = pygame.Rect(0, 0, 400, 36)
+    textRect = pygame.Rect(0, 0, 400, 120)
+    textSurface = surface.convert_alpha()
+    textSurface.fill(ALPHA)
+    textWrap.drawText(textSurface, 'This is the soundboard for the 2013 Twist-o-Rama entry, The Wedding Cake.\n \nSPACE = Door Sound', BLACK, textRect, fontObj, True)
     while running: # main program loop
-        for event in pygame.event.get():
-            if event.type == QUIT: #handle the QUIT event
-                pygame.quit()
-                sys.exit()
-            elif event.type == DISPLAY_REFRESH:
-                surface.fill(WHITE)
-                textWrap.drawText(surface, 'This is the soundboard\n for the 2013 Twist-o-Rama entry, The Wedding Cake.', BLACK, textRect, fontObj, True)
-                pygame.display.update()
-            pygame.time.wait(0)
+        event = pygame.event.wait()
+        if event.type == QUIT: #handle the QUIT event
+            pygame.quit()
+            sys.exit()
+        elif event.type == DISPLAY_REFRESH:
+            surface.fill(WHITE)
+            surface.blit(textSurface, textRect) 
+            pygame.display.update()
         
 if __name__ == '__main__':
     main()
