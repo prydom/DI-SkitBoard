3cfaf56349630a81d6dd7a49b9219e5d836e2daa
Jordan Pryde <prydom@gmail.com>
2013-04-11 22:27:41 -0700

    drawing font object

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame, sys
 from pygame.locals import *
 
+# set some basic color constants
+BLACK = (  0,   0,   0)
+WHITE = (255, 255, 255)
+RED = (255,   0,   0)
+GREEN = (  0, 255,   0)
+BLUE = (  0,   0, 255)
+
 # init all modules with defualt settings
 pygame.init()
 
@@ -10,7 +17,15 @@ DISPLAYSURF = pygame.display.set_mode((400,300))
 # set the window title
 pygame.display.set_caption("DI-SkitBoard")
 
+#display some instructions
+fontObj = pygame.font.Font('C:\Windows\Fonts\lucon.TTF', 12) # this causes a windows dependence!!!
+textInstuctionsSurface = fontObj.render('This is a test', True, BLACK)
+textRectObj = textInstuctionsSurface.get_rect()
+textRectObj.center = (200,150)
+
 while True: # main program loop
+	DISPLAYSURF.fill(WHITE)
+	DISPLAYSURF.blit(textInstuctionsSurface, textRectObj)
 	for event in pygame.event.get():
 		if event.type == QUIT: #handle the QUIT event
 			pygame.quit()
