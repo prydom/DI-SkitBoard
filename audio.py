2c8c903f6dad0de6d96780e2252910ec281d395c
Jordan Pryde <prydom@gmail.com>
2013-04-12 09:12:32 -0700

    started audio.py and coded music_play_next()

---
+++ audio.py
@@ -0,0 +1,22 @@
+from pygame.locals import *
+import pygame
+
+SND_PATH = 'D:\\Dropbox\\Sound Effects and Music for DI\\'
+
+# Music list
+music_files = ['1mellow.mp3', '2suspence.wav', '3Questioning.mp3', '4conflict.mp3',
+                '5Cheerful.mp3', '6Tragic.mp3']
+current_song = -1
+
+def music_play_next():
+    global current_song
+    if current_song < len(music_files):
+        current_song += 1
+    else:
+        return None
+    next_song = SND_PATH + music_files[current_song]
+    if pygame.mixer.music.get_busy():
+        pygame.mixer.music.fadeout(500)
+    pygame.mixer.music.load(next_song)
+    pygame.mixer.music.play(-1)
+    
