6dc0260b773042aa205b01cb324bbe02ad852c56
Jordan Pryde <prydom@gmail.com>
2013-04-11 21:58:11 -0700

    made game framework
    Things like main loop and importing simple stuff.

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-import pygame
+import pygame, sys
 from pygame.locals import *
 
 # init all modules with defualt settings
 pygame.init()
 
+# get a pygame.surface for the window
+DISPLAYSURF = pygame.display.set_mode((400,300))
+
+# set the window title
+pygame.display.set_caption("DI-SkitBoard")
+
+while True: # main program loop
+	for event in pygame.event.get():
+		if event.type == QUIT: #handle the QUIT event
+			pygame.quit()
+			sys.exit()
+		pygame.display.update()
+		
