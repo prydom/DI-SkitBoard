1c66c63c8e5e564ba7f6532d0429e22332c998cb
Jordan Pryde <prydom@gmail.com>
2013-04-12 09:40:47 -0700

    music enhancements and door sound effect

--- audio.py
+++ audio.py
@@ -1,12 +1,19 @@
 from pygame.locals import *
 import pygame
 
+# init mixer
+pygame.mixer.init()
+
 SND_PATH = 'D:\\Dropbox\\Sound Effects and Music for DI\\'
 
-# Music list
+# Music
 music_files = ['1mellow.mp3', '2suspence.wav', '3Questioning.mp3', '4conflict.mp3',
                 '5Cheerful.mp3', '6Tragic.mp3']
 current_song = -1
+MUSIC_VOLUME = 0.2
+
+# Sound effects
+soundDoor = pygame.mixer.Sound(SND_PATH + "#door.wav")
 
 def music_play_next():
     global current_song
@@ -18,9 +25,10 @@ def music_play_next():
     if pygame.mixer.music.get_busy():
         pygame.mixer.music.fadeout(500)
     pygame.mixer.music.load(next_song)
+    pygame.mixer.music.set_volume(MUSIC_VOLUME)
     pygame.mixer.music.play(-1)
 
-def music_play_back():
+def music_play_previous():
     global current_song
     if current_song > 0:
         current_song -= 1
@@ -30,6 +38,7 @@ def music_play_back():
     if pygame.mixer.music.get_busy():
         pygame.mixer.music.fadeout(500)
     pygame.mixer.music.load(next_song)
+    pygame.mixer.music.set_volume(MUSIC_VOLUME)
     pygame.mixer.music.play(-1)
     
 def music_pause():
@@ -37,3 +46,9 @@ def music_pause():
     
 def music_unpause():
     pygame.mixer.music.unpause()
+
+def music_active():
+    return pygame.mixer.music.get_busy()
+    
+def door_play():
+    soundDoor.play()
